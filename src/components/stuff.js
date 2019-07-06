c4ba6160d16b38739495b04ddc770e7538c26482
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-07-06 17:31:17 -0500

    changed route order for fix

--- src/components/stuff.js
+++ src/components/stuff.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Stuff extends React.Component {
   render() {
-    const singleLink = '/single/123456';
+    const singleLink = '/stuff/123456';
     const editLink = '/edit/123456';
     return (
       <div className="Stuff">
         <h3>Stuff</h3>
-        <NavLink to={singleLink} className="btn btn-outline-dark nav-item nav-link mb-3">Single</NavLink>
-        <NavLink to={editLink} className="btn btn-outline-dark nav-item nav-link">Edit</NavLink>
+        <Link to={singleLink} className="btn btn-outline-dark nav-item nav-link mb-3">Single</Link>
+        <Link to={editLink} className="btn btn-outline-dark nav-item nav-link">Edit</Link>
       </div>
     );
   }
